6ed24ef3078c1c7616b794e9ede112ceb15ab679
Sam Kopp <33531798+MrPilotMan@users.noreply.github.com>
2020-05-07 07:44:44 -0400

    Code clean up

--- quickip.sh
+++ quickip.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BOLD="\e[1m"; ESC="\e[0m"; GREEN="\e[32m"; RED="\e[31m";
+
 allip() {
     case $1 in
         -m) pubip -m && locip -m ;;
@@ -9,102 +11,76 @@ allip() {
     esac
 }
 
+pubip() {
+    TITLE=$BOLD"Public IP Address:"$ESC
+
+    ip_address=`dig +short myip.opendns.com @resolver1.opendns.com`
+    case $1 in
+        -m) echo $ip_address ;;
+	      "") echo -e "$TITLE" && echo -e "\t$ip_address\t(`geoiplookup $ip_address | grep -oP ", \K.*"`)" ;;
+        *) echo -e $__pubip_usage ;;
+    esac
+}
+
+locip() {
+    TITLE=$BOLD"Local IP Address(es):"$ESC
+
+    ifjson=`ip -family inet -json address show`
+    readarray -td ',' ifnames < <(printf '%s' "`echo $ifjson | jq --raw-output '[.[].ifname] | join(",")'`")
+    readarray -td ',' localaddrs < <(printf '%s' "`echo $ifjson | jq --raw-output '[.[].addr_info[].local] | join(",")'`")
+    readarray -td ',' operstates < <(printf '%s' "`echo $ifjson | jq --raw-output '[.[].operstate] | join(",")'`")
+
+    # Find the length of the longest string in each array for use in formatting the printed table.
+    ifmaxlen=0; admaxlen=0; stmaxlen=0;
+    for j in ${!ifnames[@]}; do
+        [[ ${#ifnames[j]} -gt $ifmaxlen ]] && ifmaxlen=${#ifnames[j]}
+        [[ ${#localaddrs[j]} -gt $admaxlen ]] && admaxlen=${#localaddrs[j]}
+        [[ ${#operstates[j]} -gt $stmaxlen ]] && stmaxlen=${#operstates[j]}
+    done
+
+    case $1 in
+        -d | -u | "")
+            echo -e $TITLE
+
+            for i in "${!ifnames[@]}"; do
+                case $1 in
+                    -d)
+                        [[ "${operstates[$i]}" == "DOWN" ]] && \
+                        printf "\t%-"$ifmaxlen"s\t %-"$admaxlen"s\t "$RED"DOWN"$ESC"\n" \
+                        ${ifnames[$i]} ${localaddrs[$i]}
+                        ;;
+                    -u)
+                        [[ "${operstates[$i]}" == "UP" ]] && \
+                        printf "\t%-"$ifmaxlen"s\t %-"$admaxlen"s\t "$GREEN"UP"$ESC"\n" \
+                        ${ifnames[$i]} ${localaddrs[$i]}
+                        ;;
+                    "")
+                        case ${operstates[$i]} in
+                            UP) STATUS_COLOR=$GREEN ;;
+                            DOWN) STATUS_COLOR=$RED ;;
+                            *) STATUS_COLOR="" ;;
+                        esac
+                        printf "\t%-"$ifmaxlen"s\t %-"$admaxlen"s\t "$STATUS_COLOR"%-"$stmaxlen"s"$ESC"\n" \
+                        ${ifnames[$i]} ${localaddrs[$i]} ${operstates[$i]}
+                        ;;
+                esac
+            done
+            ;;
+
+        -m) for i in "${!ifnames[@]}"; do printf "%-"$ifmaxlen"s\t %-"$admaxlen"s\n" ${ifnames[$i]} ${localaddrs[$i]}; done ;;
+        -M) printf "%s\n" ${localaddrs[@]} ;;
+        *) echo -e $__locip_usage ;;
+    esac
+}
+
 __allip_usage="
 Usage: allip [ -m ]\n
 \t-m\tMinimal - Equivalent to \`pubip -m && locip -m\`.\n
 \t-M\tExtra Minimal - Equivalent to \`pubip -m && locip -M\`."
 
-pubip() {
-    TITLE="\e[1mPublic IP Address:\e[0m"
-    ip_address=`dig +short myip.opendns.com @resolver1.opendns.com`
-
-    case $1 in
-        -m) echo $ip_address ;;
-        "")
-            ip_location=`geoiplookup $ip_address | grep -oP ", \K.*"`
-            echo -e "$TITLE\n\t$ip_address\t($ip_location)"
-            ;;
-        *) echo -e $__pubip_usage ;;
-    esac
-}
-
 __pubip_usage="
 Usage: pubip [ -m ]\n
 \t-m\tMinimal - Only show IP address."
-
-locip() {
-    TITLE="\e[1mLocal IP Address(es):\e[0m"  # Bold header.
-    GREEN="\e[32m"
-    RED="\e[31m"
-    ESC="\e[0m"
-
-    ifjson=`ip -family inet -json address show`
-
-    readarray -td ',' ifnames < <(printf '%s' "`echo $ifjson | jq --raw-output '[.[].ifname] | join(",")'`")
-    readarray -td ',' localaddrs < <(printf '%s' "`echo $ifjson | jq --raw-output '[.[].addr_info[].local] | join(",")'`")
-    readarray -td ',' operstates < <(printf '%s' "`echo $ifjson | jq --raw-output '[.[].operstate] | join(",")'`")
-
-    ifmaxlen=0
-    admaxlen=0
-    stmaxlen=0
-
-    # Find the length of the longest string in each array for use in formatting the printed table.
-    for j in ${!ifnames[@]}; do
-        iflen=${#ifnames[j]}
-        adlen=${#localaddrs[j]}
-        stlen=${#operstates[j]}
-
-        if [[ $iflen -gt $ifmaxlen ]]; then ifmaxlen=$iflen; fi;
-        if [[ $adlen -gt $admaxlen ]]; then admaxlen=$adlen; fi;
-        if [[ $stlen -gt $stmaxlen ]]; then stmaxlen=$stlen; fi;
-    done
-
-    case $1 in
-        -d)
-            echo -e $TITLE
-            for i in "${!ifnames[@]}"; do
-                if [[ "${operstates[$i]}" == "DOWN" ]]; then
-                    printf "\t%-"$ifmaxlen"s\t %-"$admaxlen"s\t "$RED"DOWN"$ESC"\n" ${ifnames[$i]} ${localaddrs[$i]}
-                fi
-            done
-            ;;
-
-        -m)
-            for i in "${!ifnames[@]}"; do
-                printf "%-"$ifmaxlen"s\t %-"$admaxlen"s\n" ${ifnames[$i]} ${localaddrs[$i]}
-            done
-            ;;
-
-        -M)
-            for address in ${localaddrs[@]}; do
-                echo $address
-            done
-            ;;
-
-        -u)
-            echo -e $TITLE;
-            for i in "${!ifnames[@]}"; do
-                if [[ "${operstates[$i]}" == "UP" ]]; then
-                    printf "\t%-"$ifmaxlen"s\t %-"$admaxlen"s\t "$GREEN"UP"$ESC"\n" ${ifnames[$i]} ${localaddrs[$i]}
-                fi
-            done
-            ;;
-
-        "")
-            echo -e $TITLE;
-            for i in "${!ifnames[@]}"; do
-                STCOLOR=""
-                case ${operstates[$i]} in
-                    UP) STCOLOR=$GREEN ;;
-                    DOWN) STCOLOR=$RED ;;
-                esac
-                printf "\t%-"$ifmaxlen"s\t %-"$admaxlen"s\t "$STCOLOR"%-"$stmaxlen"s"$ESC"\n" ${ifnames[$i]} ${localaddrs[$i]} ${operstates[$i]}
-            done
-            ;;
-
-        *) echo -e $__locip_usage ;;
-    esac
-}
 
 __locip_usage="
 Usage: locip [ OPTION ]\n
